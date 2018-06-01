d21c748314859ef6e4da8759d57c4566e2ef89a4
Jeremy Holman <jeremy.holman@gmail.com>
2018-06-01 01:14:36 -0700

    move things into directories

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,40 @@
+const webpack = require('webpack');
+const path = require('path');
+
 const config = {
-  entry: [
-    './index.js',
-  ],
+  mode: 'development',
+  entry: './src/index.js',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'src/public'),
+    host: '0.0.0.0',
+    proxy: {
+      '/api': 'http://localhost:8081'
+    }
+  },
   module: {
     rules: [
+
+      // https://webpack.js.org/loaders/babel-loader/
       {
-        test: /\.js$/,
-        loaders: [
-          'babel-loader',
-        ],
+        test: /\.jsx?$/,
         exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
       },
-    ],
+
+      // // https://webpack.js.org/loaders/sass-loader/
+      // {
+      //   test: /\.(c|sa|sc)ss$/,
+      //   use: [{
+      //     loader: 'style-loader'
+      //   }, {
+      //     loader: 'css-loader'
+      //   }, {
+      //     loader: 'sass-loader'
+      //   }]
+      // }
+    ]
   },
 };
 
