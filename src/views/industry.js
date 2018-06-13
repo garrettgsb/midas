7dd8163ef4957550898f3afdba38f0283eec9e38
Myles Rademaker <mylesprademaker@gmail.com>
2018-06-13 00:38:03 -0700

    Refs #4 added industries model and industry view to render spinach garder

--- src/views/industry.js
+++ src/views/industry.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import { Button, Counter } from './lib';
 
-export default class Industry extends React.Component {
+export default class Industries extends React.Component {
+  constructor(props) {
+    super(props);
+    this.industries = this.props.industries;
+    this.onBuild = this.props.onBuild;
+  }
+
   render() {
     return (
-      <div className='container-v'>
-        <h1>Industry</h1>
-        <div className='container alchemy'>
-          <p>I own a mine!</p>
+        <div className='container-v'>
+          <h1>Industries</h1>
+          <div className='container industry'>
+            {Object.entries(this.industries).map(industry => <IndustryPanel industry={industry[1]}/>)}
+          </div>
         </div>
+    );
+  };
+};
+
+class IndustryPanel extends React.Component {
+  render() {
+    const industry = this.props.industry;
+    return (
+      <div className='panel'>
+        <Counter label={industry.label} quantity={industry.quantity} />
+        <Counter label='Max' quantity={industry.maxQuantity} />
+          <Button
+            label='Expand'
+            clickAction={industry.build.bind(industry)}
+          />
       </div>
     );
   };
