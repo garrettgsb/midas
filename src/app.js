75dbdcdd6319175070f861a0bba09dac7bfd46de
Garrett <garrettgsb@gmail.com>
2018-09-27 13:06:54 -0700

    Deleted a bunch of stuff that I think we don't need anymore. I wonder if I'm right.

--- src/app.js
+++ src/app.js
@@ -1,95 +1,16 @@
 import autobind from 'autobind-decorator';
 import React from 'react';
 
-import Apprentice from './models/apprentices.js';
-import bind_industries from './models/industries';
-import predi_resource_pool from './models/resourcePool';
-import bind_items from './models/items';
-
-import Debug from './views/debug.js';
-import Resources from './views/resources';
-import Industry from './views/industry';
-
 require('./styles/style.css');
-
-window.debug = window.debug || { hax: true };      // debugging hackery
 
 class App extends React.Component {
   constructor() {
     super();
-    const fu = this.forceUpdate.bind(this);
-    this.state = {
-      RPOT: new RelentlessPassageOfTime(fu),
-      resources: {},
-      items: {},
-      industries: [],
-      apprentices: [],
-      maxGold: 0,
-      amAssigning: false,
-    };
-
-    const ResourcePool = predi_resource_pool(fu, this.state);
-    this.state.resources = new ResourcePool();
-
-    this.state.items = bind_items(fu, this.state);
-    this.state.industries = bind_industries(fu, this.state);
-    this.state.resources.lead.set(9, 'init');
-    this.state.resources.thalers.set(10, 'init');
-    this.state.RPOT.run();
-
-    // debugging hackery
-    window.globalState = this.state;
-  }
-
-  @autobind
-  hireApprentice() {
-    this.setState({ apprentices: [...this.state.apprentices, new Apprentice(this.state.RPOT)] });
-  }
-
-  transmute(from, to) {
-    if (from.transmute(to)) {
-      to.quantity += 1;
-    }
-  }
-
-  @autobind
-  assign_toggle(appr) {
-    if (!this.state.amAssigning) {
-      this.setState({
-        amAssigning: appr,
-        pending_assignment: [],
-      });
-    } else {
-      this.assign_finish(appr, this.state.pending_assignment);
-      this.setState({
-        amAssigning: false,
-        pending_assignment: [],
-      });
-    }
-  }
-
-  assign_finish(appr, proposed_assignments) {
-    appr.assign(proposed_assignments);
-  }
-
-  @autobind
-  assign_append(fn) {
-    this.setState({ pending_assignment: [...this.state.pending_assignment, fn] });
   }
 
   render() {
     return (
-      <div className='container-v'>
-        { window.debug.hax && <Debug/> }
-        <Resources
-          resources={this.state.resources}
-        />
-        <Industry
-          industries={this.state.industries}
-          resources={this.state.resources}
-          assigning={this.state.amAssigning ? this.assign_append : undefined}
-        />
-      </div>
+      <p>🐢</p>
     );
   }
 }
