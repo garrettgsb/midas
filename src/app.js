e83d4a2ad75797cd5bd9b55ed0dec1b8808d8997
Garrett <garrettgsb@gmail.com>
2018-10-02 21:22:03 -0700

    Prototype. Hand-off. Go\!

--- src/app.js
+++ src/app.js
@@ -6,24 +6,29 @@ require('./styles/style.css');
 class App extends React.Component {
   constructor() {
     super();
+    this.toggleModal = () => this.setState(prev => ({ theModalIsOpen: !prev.theModalIsOpen }));
     this.state = {
       // Mock map objects
-      mapIcons: [
-        { id: (''+Math.random()).slice(2,10), pos: { x: 10, y: 80 }, color: '#3399BB', icon: '🏠', clickAction: () => alert('You clicked me') },
-        { id: (''+Math.random()).slice(2,10), pos: { x: 60, y: 140 }, color: '#99BB33', icon: '⛪️', clickAction: () => alert('You clicked me') },
-        { id: (''+Math.random()).slice(2,10), pos: { x: 110, y: 40 }, color: '#BB3399', icon: '🏪', clickAction: () => alert('You clicked me') },
+      items: [
+        { id: (''+Math.random()).slice(2,10), pos: { x: 10, y: 80 }, color: '#3399BB', icon: '🏠', clickAction: this.toggleModal, modalData: 'A house' },
+        { id: (''+Math.random()).slice(2,10), pos: { x: 60, y: 140 }, color: '#99BB33', icon: '⛪️', clickAction: this.toggleModal, modalData: 'A church' },
+        { id: (''+Math.random()).slice(2,10), pos: { x: 110, y: 40 }, color: '#BB3399', icon: '🏪', clickAction: this.toggleModal, modalData: 'A 24 store' },
       ],
+      theModalIsOpen: true,
     };
+  }
+
+  @autobind
+  changeModalTo(id) {
+
   }
 
   render() {
     return (
       <div className='container'>
-        <p>🐢</p>
-        <Map icons={this.state.mapIcons} />
-        {/* <Sidebar>
-          <SidebarItem/>
-        </Sidebar> */}
+        <Map items={this.state.items} />
+        <Sidebar items={this.state.items} />
+        <TheModal isOpen={this.state.theModalIsOpen} />
       </div>
     );
   }
@@ -32,8 +37,8 @@ class App extends React.Component {
 class Map extends React.Component {
   render() {
     return (
-      <svg viewBox="0 0 80% 400" xmlns="http://www.w3.org/2000/svg" style={{width: '80%', height: 400}}>
-        { this.props.icons.map(icon => <MapIcon key={icon.id} icon={icon}/>) }
+      <svg viewBox="0 0 80 400" xmlns="http://www.w3.org/2000/svg" style={{width: '80%', height: 400}}>
+        { this.props.items.map(item => <MapIcon key={item.id} item={item}/>) }
       </svg>
     );
   }
@@ -41,9 +46,42 @@ class Map extends React.Component {
 
 class MapIcon extends React.Component {
   render() {
-    console.log(this.props.icon);
+    const { color, icon, pos, clickAction } = this.props.item;
     return (
-      <circle cx={this.props.icon.pos.x} cy={this.props.icon.pos.y} r="10" style={{ fill: this.props.icon.color}} />
+      <circle onClick={clickAction} cx={pos.x} cy={pos.y} r="10" style={{ fill: color }}>
+        {icon} {/* TODO: Make this show up */}
+      </circle>
+    )
+  }
+}
+
+class Sidebar extends React.Component {
+  render() {
+    return (
+      <div className='container-v'>
+        { this.props.items.map(item => <SidebarItem item={item} />) }
+      </div>
+    )
+  }
+}
+
+class SidebarItem extends React.Component {
+  render() {
+    const { color, icon, clickAction } = this.props.item;
+    return (
+      <div onClick={clickAction} className='sidebar-item' style={{backgroundColor: color, minWidth: '100px'}}>
+        <p>🐢</p>
+      </div>
+    );
+  }
+}
+
+class TheModal extends React.Component {
+  render() {
+    return (
+      <div className={`the-modal ${this.props.isOpen ? '' : 'hidden'}`}>
+        <p>🐢</p>
+      </div>
     )
   }
 }
